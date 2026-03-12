15a9d3673c0f4fe83635d11978d41fa61338c160
adithya <ruthvik.22mis7255@vitapstudent.ac.in>
2026-03-12 12:02:06 +0530

    Initial commit - College Admission Predictor ML Web App

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, render_template
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+app = Flask(__name__)
+
+# Load dataset
+df = pd.read_csv("admission_predict.csv")
+
+# Remove extra spaces in column names
+df.columns = df.columns.str.strip()
+
+# Features and target
+X = df[['GRE Score','TOEFL Score','University Rating','SOP','LOR','CGPA','Research']]
+y = df['Chance of Admit']
+
+# Train model
+model = LinearRegression()
+model.fit(X, y)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    gre = float(request.form['gre'])
+    toefl = float(request.form['toefl'])
+    rating = float(request.form['rating'])
+    sop = float(request.form['sop'])
+    lor = float(request.form['lor'])
+    cgpa = float(request.form['cgpa'])
+    research = float(request.form['research'])
+
+    input_data = [[gre, toefl, rating, sop, lor, cgpa, research]]
+
+    prediction = model.predict(input_data)
+
+    result = round(prediction[0] * 100, 2)
+
+    return render_template("result.html", prediction=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
